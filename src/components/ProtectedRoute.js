9abc5de9b6a0b2366689cb938d6496d04dd9f3f4
shpi1 <shpi@ciklum.com>
2024-12-30 11:00:54 +0530

    code clean up

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,14 @@
-// src/components/ProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { isAuthenticated } from '../utils/auth';
 
 const ProtectedRoute = ({ children }) => {
-    return isAuthenticated() ? children : <Navigate to="/login" />;
+    if (isAuthenticated()) {
+        if (localStorage.getItem('role') === 'admin') {
+            return children;
+        }
+    }
+    return <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
